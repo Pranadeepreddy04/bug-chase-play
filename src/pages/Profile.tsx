4b20bbd691daedeb3245504134e0136d662a95ec
Jacob Koplik <jkoplik@albany.edu>
2025-11-19 01:15:47 -0500

    Added some styles for the profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -11,13 +11,11 @@ import {Container, Row, Col, CardBody} from 'react-bootstrap';
 const Profile = () => {
   return (
 
-    
-
     <>
 
-    {/* next thing to try to do: get the background looking good 
-    -- **we also need to color the logos/images correctly
-    */}
+    {/* Let's try to place everything inside a main div - step by step */}
+    {/* Oh, ok maybe the issue was that I had a classname on the container? Now we can just continue working outer to inner on this*/}
+    <div className="min-h-screen bg-background relative">
       <Container>
         <Row>
           {/* Logo image - reference from header*/}
@@ -42,8 +40,15 @@ const Profile = () => {
 
       </Container>
 
-      <Container className = "bottomContainer">
-        <Card>
+    {/* next thing to try to do: get the background looking good 
+    -- **we also need to color the logos/images correctly
+    */}
+      
+      
+
+      <Container>
+        {/* Outer card in the 2nd class - we want this to have width 50%*/}
+        <Card style = {{width : '50%'}}>
           {/*We need 3 sub-cards, each of which having the same style*/}
 
           {
@@ -52,7 +57,15 @@ const Profile = () => {
 
           {/* Issue/TODO: the css sheet is still not working, so I might want to figure that out (for now, I'm just repeating styles in here) */}
 
-          <Card className = "profileStatCard" style = {{backgroundColor : 'rgba(151, 132, 132, 1)', width: '50%'}}>
+          {/*Div for around the inner cards 
+          Q: are these widths and height relative to the nearest div?
+          A: actually parent element
+          But then why isn't it working with respect to the card? (particularly the height isn't)
+          */}
+
+
+          <div style = {{width : '90%', height : '20%', margin: 'auto'}}>
+            <Card className = "profileStatCard" style = {{backgroundColor : 'rgba(151, 132, 132, 1)', width: '50%'}}>
             <CardHeader>
               <b>Wins as Tester</b>
             </CardHeader>
@@ -60,34 +73,39 @@ const Profile = () => {
               <p>&emsp; 10</p>
             </CardBody>
             
-          </Card>
-          {/* Issue: can we make another card that has the same classname?? Yes, let's go*/}
-          <Card className = "profileStatCard" style = {{backgroundColor : 'gray'}}>
-            <CardHeader>
-              <b>Wins as Saboteur</b>
-            </CardHeader>
-            <CardBody>
-              <p>&emsp; 10</p>
-            </CardBody>
-          </Card>
+            </Card>
+            {/* Issue: can we make another card that has the same classname?? Yes, let's go*/}
+            <Card className = "profileStatCard" style = {{backgroundColor : 'gray'}}>
+              <CardHeader>
+                <b>Wins as Saboteur</b>
+              </CardHeader>
+              <CardBody>
+                <p>&emsp; 10</p>
+              </CardBody>
+            </Card>
 
-          <Card className = "profileStatCard" style = {{backgroundColor : 'gray'}}>
-            <CardHeader>
-              <b>Total Number of Games Played</b>
-            </CardHeader>
-            <CardBody>
-              <p>&emsp; 10</p>
-            </CardBody>
-          </Card>
+            <Card className = "profileStatCard" style = {{backgroundColor : 'gray'}}>
+              <CardHeader>
+                <b>Total Number of Games Played</b>
+              </CardHeader>
+              <CardBody>
+                <p>&emsp; 10</p>
+              </CardBody>
+            </Card>
+          </div>
+          
 
           {/* Another potential to-do: make these sub-cards functions, since they keep getting repeated?? */}
 
 
         </Card>
       </Container>
-    </> 
 
-  )
+      </div>
+
+    </>
+
+  );
 }
 
 export default Profile;
