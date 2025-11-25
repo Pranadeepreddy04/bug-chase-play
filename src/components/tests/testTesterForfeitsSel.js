75c68dca7f04b9c9baee3eb0e75b1d19ab1c8a5c
Jacob Koplik <jkoplik@albany.edu>
2025-11-24 20:01:14 -0500

    I think I made a good test for the hedaer page

--- src/components/tests/testTesterForfeitsSel.js
+++ src/components/tests/testTesterForfeitsSel.js
@@ -2,9 +2,12 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
 import 'selenium-webdriver/chrome.js';
 
+import assert from 'node:assert';
+
 
 // How to run this...
 
+// Practice Test function
 async function testButton() {
 
         
@@ -32,10 +35,18 @@ async function testButton() {
                 // locate each button separately, then click on it using the .click method after
 
                 const button = await driver.wait(until.elementLocated(By.id('startDuel')), 10000); // Replace 'myButton' with your button's ID
+                // ok, so yes, we do have to go to another page for this to be located
+
+                console.log("Trying log log the raw button");
+                console.log(button);
 
                 console.log("What does the button say");
                 const buttonText = await button.getText();
                 console.log(buttonText);
+
+                // test: let's make an assertion for what the button text should say
+                assert.strictEqual(buttonText, "Mark as Complete");
+
 
 
                 await button.click();
@@ -57,6 +68,76 @@ async function testButton() {
         
 
         }
-    testButton();
+
+
+    // Test Function 1: header page (signed in OR singed out - type: very simple unit test...)
+
+    async function testHeaderPage() {
+
+        let driver = await new Builder().forBrowser('chrome').build();
+
+        // let's store the buttons OUTSIDE the error thing may - so we can still find them after we get an error
+
+
+        // note: let creates a mutable variable
+        let loginButton = null;
+        let signoutButton = null;
+
+
+        try {
+
+            await driver.get('http://192.168.1.167:8080/');
+            // First, we're trying to make sure that the header has EITHER login or sign-out on it
+            loginButton = await driver.wait(until.elementLocated(By.id('loginButton')), 10000);
+            // test: what if one of the buttons is for start duel??
+            
+
+            // ok, we found the buttons.
+            // big issue though: what if the first button fails. Then, is it possible to get the 2nd one?
+
+            // so maybe we can do a try/catch individually for these??
+
+            
+
+        }
+        // I think we need to try to catch errors in here, since we are doing an either-or for the button
+        catch (error) {
+            // do nothing yet??
+        }
+        try {
+            signoutButton = await driver.wait(until.elementLocated(By.id('signOutButton')), 10000);
+        }
+        catch (error){
+
+        }
+
+
+        finally {
+
+            // let's test if the buttons are undefined (but do we need to first store the variable as a loner before we can guarantee printing ability?)
+            console.log("In the finally section - here is the login or the signup button (raw buttons);");
+            // ok, that worked...so I need a way to SWITCH the context to the header page...
+
+            console.log(loginButton);
+            console.log(signoutButton);
+
+            // final step for this test: we want to assert that one of them is not null
+            
+            // let's create a boolean for this first
+            const oneIsNotNull = (loginButton != null) || (signoutButton != null);
+
+            // now, let's try to do an assertTrue
+            assert.equal(oneIsNotNull, true);
+
+
+
+            await driver.quit();
+        }
+
+    }
+
+
+    //testButton();
+    testHeaderPage();
 
 
